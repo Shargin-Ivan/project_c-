2d367c9d53a0834d368ce67369d7c8853f0f75b0
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-05 21:41:00 +0300

    Add test3

---
+++ test3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите числовое значение: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num0 = 0 - num;
+while (num0 <= num)
+{
+    Console.Write(num0);
+    Console.Write(" ");
+    num0++;
+}
