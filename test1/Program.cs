5689bdd20174ebc5bb060b165ae10e41b47d8023
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-05 21:12:00 +0300

    + test1

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == (num2 * num2))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
