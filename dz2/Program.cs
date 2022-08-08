e95baa23b2244a2ab05a01e26db81c01dfc1021c
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-08 18:25:50 +0300

    Add dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine($"Максимальное число: {num1}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число: {num3}");
+    }
+}
+else
+{
+    if (num2 > num3)
+    {
+        Console.WriteLine($"Максимальное число: {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число: {num3}");
+    }
+}
