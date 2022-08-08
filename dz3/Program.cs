35503617bd67f5c72f6235c13639238f1e28cf29
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-08 18:26:12 +0300

    Add dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Чётно ли следующее число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num % 2) == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
