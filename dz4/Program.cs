c82fa13d7b0a22f59e83564aa4f1da5b6b784c53
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-08 18:26:39 +0300

    Add dz4

---
+++ dz4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("До какого числа вывести чётные от 1? До: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int cnt = 2;
+while (cnt <= num)
+{
+    Console.Write($"{cnt} ");
+    cnt = cnt + 2;
+}
