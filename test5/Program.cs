eae960f006b16093101393ba9521e4b7a3174187
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-09 20:55:33 +0300

    Add test5

---
+++ test5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int num = new Random().Next(10, 100);
+
+int a = num / 10;
+int b = num % 10;
+
+int max = a;
+
+if (max < b) max = b;
+
+Console.WriteLine(num);
+Console.WriteLine(max);
