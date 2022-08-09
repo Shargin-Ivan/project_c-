d63f6609dce784bc6190ebc0c939d6c4b2d3b126
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-09 21:18:48 +0300

    Add test7

---
+++ test7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"Число {num1} кратно числу {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не кратно числу {num2} и остаток равен {num1 % num2}");
+}
