66a3caafefb3259b085b571208dbd92f8202ea4d
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-09 22:48:01 +0300

    Add test8

---
+++ test8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = 7;
+int b = 23;
+
+if (((num % a) == 0) && ((num % b) == 0))
+{
+    Console.WriteLine($"Число {num} кратно одновременно {a} и {b}");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно одновременно {a} и {b}");
+}
