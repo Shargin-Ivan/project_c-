ccdd1a90f6a3e8787cb4cc424b43bdbfe59fa102
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-09 21:01:44 +0300

    + test6

--- test6/Program.cs
+++ test6/Program.cs
@@ -3,10 +3,10 @@
 int num = new Random().Next(100, 1000);
 
 int a = num / 100;
-int b = num / 10;
+int b = (num - a * 100) / 10;
 int c = num % 10;
 
 
 
-Console.WriteLine(num);
+Console.WriteLine($"Число: {num}. Первая цифра: {a}. Вторая цифра: {b}. Третья цифра: {c}.");
 Console.WriteLine($"{a}{c}");
