9ed345cf9274b68097ce75a31f7e2de329db3e12
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-09 22:48:24 +0300

    Add test9

---
+++ test9/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if ((num1 == num2 * num2) || (num2 == num1 * num1))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
