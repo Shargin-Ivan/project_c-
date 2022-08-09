e6fbdcb2de34c92eed82061dbcbab7d8bb69d783
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-09 20:55:55 +0300

    Add test6

---
+++ test6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int num = new Random().Next(100, 1000);
+
+int a = num / 100;
+int b = num / 10;
+int c = num % 10;
+
+
+
+Console.WriteLine(num);
+Console.WriteLine($"{a}{c}");
