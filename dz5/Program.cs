b8e2daa951e3d7bf1bf50310342eda776cf2e866
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-16 00:15:09 +0300

    +dz5

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = (num - (num / 100) * 100) / 10;
+Console.WriteLine($"Вторая цифра: {result}");
