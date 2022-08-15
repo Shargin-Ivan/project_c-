7a7449a1b614a0aa3f6761d5780c94d0f81300f2
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-16 00:15:53 +0300

    Add dz7

---
+++ dz7/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите порядковый номер дня недели(от 1 до 7): ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Теперь произойдёт магия и я отвечу является ли этот день выходным: ");
+if ((num > 0) && (num < 8))
+{
+    if (num > 5)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("ОТ 1 ДО 7");
+}
