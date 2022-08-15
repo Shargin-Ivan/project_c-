9d6ea2fd3f8dbe7d4c0d65a55a438c3df27958da
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-16 00:16:09 +0300

    Add dz8

---
+++ dz8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите пятизначное число: ");
+string num = Console.ReadLine();
+if ((num[0] == num[4]) && (num[1] == num[3]))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
