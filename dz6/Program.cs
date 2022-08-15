b0f95f0c28e9ab4657cd50a49212f0025eb4d4df
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-16 00:15:33 +0300

    Add dz6

---
+++ dz6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    Console.WriteLine($"Третья цифра числа: {num % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
