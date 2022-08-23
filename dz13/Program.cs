2c9a601f47a4cb04c9926aeb559c4fedc22f4670
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-23 18:56:42 +0300

    Add dz13

---
+++ dz13/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr(int num, int x, int y)
+{
+   int[] array = new int[num];
+   for(int i = 0; i < num; i++)
+   {
+      array[i] = new Random().Next(x, y + 1);
+   }
+   return array;
+}
+int[] array = arr(8, -9, 9);
+Console.WriteLine(string.Join(", ", array));
+// Можно было сделать вывод в методе, либо создать ещё один метод,
+// но мне лень
