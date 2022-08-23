b1802c72f2aa6a31ee0380948e9a647554c67e69
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-23 18:56:04 +0300

    Add dz11

---
+++ dz11/Program.cs
@@ -0,0 +1,16 @@
+﻿int Exponentiation(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите в какую степень нужно возвести число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = Exponentiation(A, B);
+Console.Write($"Число {A} в степени {B} равно {result}");
