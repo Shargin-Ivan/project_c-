f3f4b5e6ac28fec4cf3d7bf9206911fcd8740e50
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-23 18:56:26 +0300

    Add dz12

---
+++ dz12/Program.cs
@@ -0,0 +1,14 @@
+﻿int TheSumOfTheDigitsInANumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = TheSumOfTheDigitsInANumber(num);
+Console.Write($"Сумма цифр в числе {num} равно {result}");
