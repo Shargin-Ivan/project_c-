bb3e0b7dabe11e699873642e13c63a7b6e6c8a01
Shargin-Ivan <ivanshargin@gmail.com>
2022-08-28 20:42:07 +0300

    Add dz14

---
+++ dz14/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int[] arr(int num, int x, int y)
+{
+   int[] array = new int[num];
+   for(int i = 0; i < num; i++)
+   {
+      array[i] = new Random().Next(x, y + 1);
+   }
+   return array;
+}
+int EvenNumberCounter(int[] arr)
+{
+    int cnt = 0;
+    for (int i = 0; i < arr.Length;i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+int[] array = arr(10, 100,999);
+int count = EvenNumberCounter(array);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Колличество чётных чисел: {count}");
